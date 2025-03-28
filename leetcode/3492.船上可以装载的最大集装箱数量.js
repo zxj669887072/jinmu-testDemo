583114d921fe658d64c2154ed691f31dd424d6eb
zxj669887072 <669887072@qq.com>
2025-03-28 16:59:00 +0800

    add leetcode 3492

---
+++ leetcode/3492.船上可以装载的最大集装箱数量.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=3492 lang=javascript
+ *
+ * [3492] 船上可以装载的最大集装箱数量
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} w
+ * @param {number} maxWeight
+ * @return {number}
+ */
+var maxContainers = function(n, w, maxWeight) {
+    
+};
+// @lc code=end
+
