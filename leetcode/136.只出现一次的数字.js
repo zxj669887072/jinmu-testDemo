2b32963c94d29300ad097dd79c9bdd6e70314b86
zxj669887072 <669887072@qq.com>
2025-03-28 10:04:46 +0800

    update leetcode 136

--- leetcode/136.只出现一次的数字.js
+++ leetcode/136.只出现一次的数字.js
@@ -13,7 +13,7 @@ var singleNumber = function (nums) {
   let ln = nums.length;
   if (ln === 1) return nums[0];
 
-  let obj = {};
+  /* let obj = {};
   for (let i = 0; i < ln; i++) {
     let num = nums[i];
     if (num in obj) {
@@ -22,9 +22,25 @@ var singleNumber = function (nums) {
       obj[num] = i;
     }
   }
-  return +Object.keys(obj)[0];
+  return +Object.keys(obj)[0]; */
+
+  /* nums.sort((a, b) => a - b);
+  for (let i = 0; i < ln; i += 2) {
+    if (nums[i] !== nums[i + 1]) {
+      return nums[i];
+    }
+  }
+  return nums[ln - 1]; */
+
+  return nums.reduce((a, b) => a ^ b, 0);
+
+  // let result = 0;
+  // for (const num of nums) {
+  //   result ^= num; // 异或运算消去重复项
+  // }
+  // return result;
 };
 // @lc code=end
 
 console.log(singleNumber([2, 2, 1]));
-console.log(singleNumber([4,1,2,1,2]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
